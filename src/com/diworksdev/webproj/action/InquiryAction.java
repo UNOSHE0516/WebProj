5bcc408fbdfc9e524154e596a5c8e7764d0d652a
UNOSHE0516 <hiro0516jms@gmail.com>
2025-02-10 22:21:00 +0900

    struts.xmlにinquiryComplete追加

--- src/com/diworksdev/webproj/action/InquiryAction.java
+++ src/com/diworksdev/webproj/action/InquiryAction.java
@@ -1,10 +1,9 @@
 package com.diworksdev.webproj.action;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class InquiryAction {
-	public class InquiryAction extends ActionSupport{
+public class InquiryAction extends ActionSupport{
 		public String execute(){
 			return SUCCESS;
 		}
-	}
+
 }
